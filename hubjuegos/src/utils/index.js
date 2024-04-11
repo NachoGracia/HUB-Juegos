885c89eaa4b33809ce32244fc84824af5f12ec42
NachoGracia <gracianacho@gmail.com>
2024-04-11 11:55:39 +0200

    change color export deleted

--- hubjuegos/src/utils/index.js
+++ hubjuegos/src/utils/index.js
@@ -1,10 +1,10 @@
 export * from "./initTemplate";
 export * from "./route";
-export * from "./changeColor";
+
 export * from "./axios";
 export * from "./paginacion";
 export * from "./dataPokemon";
 export * from "./typePokemon";
-//! EXPORT DE LÓGICA PARA LLEVARLO A LA PÁGINA
+
 export * from "./logicaTresEnRaya";
 export * from "./logicaPPT";
